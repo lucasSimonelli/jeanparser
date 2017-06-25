794f36e6b760ce37441a01ee832b4cd56b69e50e
lucasSimonelli <lucasp.simonelli@gmail.com>
2017-06-25 00:21:57 -0300

    Concurrency test

---
+++ README.md
@@ -0,0 +1,13 @@
+# Jean parser instructions
+
+## Install python
+        https://wiki.python.org/moin/BeginnersGuide/Download
+
+## Install pip if necessary
+        https://packaging.python.org/tutorials/installing-packages/#install-pip-setuptools-and-wheel
+
+## Install requirements (Run this command from this folder)
+        pip install -r requirements.txt
+
+## Run app
+        python jeanscrapper.py
--- jeanscrapper.py
+++ jeanscrapper.py
@@ -2,21 +2,31 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-URL='http://www.falabella.com/falabella-cl/category/cat6040035/Jeans'
-ITEMS_PER_PAGE=100
-PARAMS={'No':0, 'Nrpp':ITEMS_PER_PAGE}
+BASE_SERVER = 'http://www.falabella.com'
+URL = BASE_SERVER + '/falabella-cl/category/cat6040035/Jeans'
+ITEMS_PER_PAGE = 100
+PARAMS = {'No': 0, 'Nrpp': ITEMS_PER_PAGE}
 
 
-def parseJeans(jeans):
-    prices = []
-    for jean in jeans:
-        brand = jean.find('div', class_='marca').a['title']
-        description = jean.find('div', class_='detalle').a['title']
-        price = jean.find('div', class_='precio1').span.text
-        prices.append((unicode(brand).encode('utf-8'),
-        unicode(description).encode('utf-8'),
-        unicode(price).encode('utf-8')))
-    return prices
+def text_to_unicode(text):
+    return unicode(text).encode('utf-8')
+
+
+def parse_jeans(raw_jeans):
+    parsed_jeans = []
+    for raw_jean in raw_jeans:
+        detail_page = BASE_SERVER + raw_jean.find('div', class_='marca').a['href']
+        brand = raw_jean.find('div', class_='marca').a['title']
+        description = raw_jean.find('div', class_='detalle').a['title']
+        price = raw_jean.find('div', class_='precio1').span.text
+        parsed_jeans.append(
+            (
+                text_to_unicode(brand),
+                text_to_unicode(description),
+                text_to_unicode(price)
+            )
+        )
+    return parsed_jeans
 
 
 r = requests.get(URL, params=PARAMS)
@@ -24,13 +34,13 @@ soup = BeautifulSoup(r.text, 'html.parser')
 jeans = soup.find_all('div', class_='cajaLP4x')
 i = 0
 with open('jeans.csv', 'wb') as jeans_csv:
+    wr = csv.writer(jeans_csv)
     while len(jeans) > 0:
-        parsed_jeans = parseJeans(jeans)
-        wr = csv.writer(jeans_csv)
+        parsed_jeans = parse_jeans(jeans)
         for parsed_jean in parsed_jeans:
             wr.writerow(parsed_jean)
         i += 1
-        PARAMS['No'] = i*ITEMS_PER_PAGE
+        PARAMS['No'] = i * ITEMS_PER_PAGE
         r = requests.get(URL, params=PARAMS)
         soup = BeautifulSoup(r.text, 'html.parser')
         jeans = soup.find_all('div', class_='cajaLP4x')
